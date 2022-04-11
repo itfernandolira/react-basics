b00c55851e87cf5a4ee9d291b7b45c9bab3a8259
Fernando Lira <it.fernandolira@gmail.com>
2022-04-11 18:51:16 +0100

    Component properties - calculating values

--- src/components/basic/ComParametro.jsx
+++ src/components/basic/ComParametro.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 export default function ComParametro(props) {
     console.log(props);
+    const status = props.nota >= 10 ? 'Aprovado' : 'Não aprovado';
     return (
         <div>
             <h2>{ props.titulo }</h2>
             <p>
-                Aluno - { props.aluno } - { props.nota }
+                Aluno - { props.aluno } - { props.nota } - { status }
             </p>
         </div>
     );
