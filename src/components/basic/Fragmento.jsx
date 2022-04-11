0eef9c2333f3ef48b0c9db82e7f9eea88ed3b6ea
Fernando Lira <it.fernandolira@gmail.com>
2022-04-11 19:27:43 +0100

    React Fragment

---
+++ src/components/basic/Fragmento.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Fragmento() {
+    return (
+        <React.Fragment>
+            <h2>Fragmento</h2>
+            <p>Cuidado com o erro!</p>
+            <p>Dois elementos adjacentes têm de estar envolvidos num outro elemento</p>
+        </React.Fragment>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import React from 'react'
 
 import Primeiro from './components/basic/Primeiro'
 import ComParametro from './components/basic/ComParametro'
+import Fragmento from './components/basic/Fragmento'
 
 ReactDOM.render(
     <div>
        <Primeiro></Primeiro>
        <ComParametro titulo="Classificações" aluno="Pedro" nota={16.8}></ComParametro>
+       <Fragmento></Fragmento>
     </div>,
     document.getElementById('root')
     );
