419434267bebbe949871d4406a840b67fee85517
Fernando Lira <it.fernandolira@gmail.com>
2022-04-11 18:55:37 +0100

    Component properties - read only!

--- src/components/basic/ComParametro.jsx
+++ src/components/basic/ComParametro.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 export default function ComParametro(props) {
     console.log(props);
     const status = props.nota >= 10 ? 'Aprovado' : 'Não aprovado';
+    //Erro - as propriedades dos componentes são de leitura apenas!
+    //props.nota = Math.ceil(props.nota);
+    const notaInt = Math.ceil(props.nota);
     return (
         <div>
             <h2>{ props.titulo }</h2>
             <p>
-                Aluno - { props.aluno } - { props.nota } - { status }
+                Aluno - { props.aluno } - { notaInt } - { status }
             </p>
         </div>
     );
