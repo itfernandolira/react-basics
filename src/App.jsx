dbc2b7c48c9f5c1d6f4ecfcaa29105c969a99c66
Fernando Lira <it.fernandolira@gmail.com>
2022-04-13 00:01:04 +0100

    Card component CSS Flex - part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 
 import Primeiro from './components/basic/Primeiro'
+import './App.css'
 import ComParametro from './components/basic/ComParametro'
 import Fragmento from './components/basic/Fragmento'
 import Card from './components/layout/Card'
 
 export default () =>
-    <div>
-        <Card title="Primeiro">
-            <Primeiro></Primeiro>
-            <Primeiro></Primeiro>
-        </Card>
+    <div className='App'>
+        <div className='Cards'>
+            <Card title="Primeiro">
+                <Primeiro></Primeiro>
+                <Primeiro></Primeiro>
+            </Card>
 
-        <Card title="Com parametro">
-            <ComParametro titulo="Classificações" aluno="Pedro" nota={16.8}></ComParametro>
-        </Card>
-        
-        <Card title="Com parametro">
-            <Fragmento></Fragmento>
-        </Card>
+            <Card title="Com parametro">
+                <ComParametro titulo="Classificações" aluno="Pedro" nota={16.8}></ComParametro>
+            </Card>
+            
+            <Card title="Com parametro">
+                <Fragmento></Fragmento>
+            </Card>
+        </div>
     </div>
